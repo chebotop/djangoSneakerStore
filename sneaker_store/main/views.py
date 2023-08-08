267488bd1a93a4270c8fc5da0dcfe0be3f5263a6
Ruslan <68710700+chebotop@users.noreply.github.com>
2023-08-08 21:35:17 +0300

    upd

--- sneaker_store/main/views.py
+++ sneaker_store/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def sheets_bot(request):
+    return render(request, 'main/sheets_bot.html')
