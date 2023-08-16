a95f5db2b2c65879f32a7b33657ebc24b445d34d
Ruslan <68710700+chebotop@users.noreply.github.com>
2023-08-16 23:14:48 +0300

    It works

--- sneaker_store/telegram_bot/bot.py
+++ sneaker_store/telegram_bot/bot.py
@@ -15,31 +15,48 @@ logging.basicConfig(level=logging.INFO)
 load_dotenv()
 creds = ServiceAccountCredentials.from_json_keyfile_name('telegram_bot/data/secret_key.json')
 scopes = [os.getenv('SCOPE_SHEETS'), os.getenv('SCOPE_DRIVE')]
+gc = gspread.authorize(creds)
 
 
-def fetch_data_from_gsheets():
-    return gspread.authorize(creds).open('Cars and tip').sheet1
+class Data:
+    def __init__(self, gc):
+        self.data = gc.open('Cars and tip').sheet1
+    
+    def update_data(self, gc):  
+        self.data = gc.open('Cars and tip').sheet1
+
+    def get(self):
+        return self.data.get_all_values()
+    
+    def add_new_data(self, data):
+        self.data.append_row(data)
+    
+
+data = Data(gc)
+
 
 
 def is_number_exist(text_input):
-    data = fetch_data_from_gsheets().get_all_values()
+    object_data = data.get()
     keyword = text_input.split()[0]  # Берем только первое слово из строки, так как это номер
-    for sublist in data:
+    for sublist in object_data:
         if keyword.lower() in sublist[0].lower():
             return True
     return False
 
 
 def return_existing_data(number):
-    data = fetch_data_from_gsheets().get_all_values()
-    keyword = number.split()[0]  # Берем только первое слово из строки, так как это номер
-    for sublist in data:
+    data_list = data.get()
+    keyword = number.split(' ')[0]  # Берем только первое слово из строки, так как это номер
+    logging.info(f"Ищем запись с номером: {keyword}")
+    for sublist in data_list:
         if keyword.lower() in sublist[0].lower():
+            logging.info(f"Найдена запись: {sublist}")
             return ' '.join(sublist)
+    else:
+        return False
 
 
-def add_new_data(data):
-    fetch_data_from_gsheets().append_row(data)
 
 
 TOKEN = os.getenv('TOKEN')
@@ -60,6 +77,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def process_args(message: types.Message, state: FSMContext):
     lines = message.text.split('\n')
+    logging.info(f"Получены аргументы: {lines}")
     added_count = 0
     
     for line in lines:
@@ -67,18 +85,22 @@ async def process_args(message: types.Message, state: FSMContext):
         number = args[0]
         
         if len(args) == 3 and not is_number_exist(number):
-            add_new_data(args)
+            logging.info(f"Number: {number}, Respond: {is_number_exist(number)}")
+            data.add_new_data(args)
             added_count += 1
         elif len(args) == 3 and is_number_exist(number):
             previous_data = return_existing_data(number)
-            await message.answer(f"Запись с номером '{number}' уже существует и была проигнорирована.\nПрошлая запись: {previous_data}")
-        else:
-            await message.answer(f"Строка '{line}' не соответствует формату и была пропущена.")
-
+            await message.answer(f"есть запись: {previous_data}")
     if added_count:
         await message.answer(f"Добавлено {added_count} записей.")
     else:
-        await message.answer("Не было добавлено ни одной записи.")
+        number = message.text.split(' ')[0]
+        if return_existing_data(number):
+            await message.answer(f"Запись существует. Данные: {return_existing_data(number)}")
+        else:
+            await message.answer(f"По записи {number} нет данных")
+
+
 
 
 def run_bot():
