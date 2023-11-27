bec2e67d22f9d8f2987adb1a127ecde13fad6601
Ruslan <68710700+chebotop@users.noreply.github.com>
2023-11-27 03:27:24 +0300

    размеры отправляются в корзину

--- sneaker_store/main/static/js/size-selection.js
+++ sneaker_store/main/static/js/size-selection.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
             this.classList.add('size-selected');
 
             // Получить и обновить размер
-            var selectedSize = this.innerText.trim().split(' ')[0];
+            var selectedSize = this.innerText.trim().split('\n')[0];
             console.log("Selected size:", selectedSize);  // Отладочный вывод
             document.getElementById('selected_size').value = selectedSize;
         });
