e728490deb4dcdab9277a2e7ce0d310c3f0a42fb
Ruslan <68710700+chebotop@users.noreply.github.com>
2024-01-10 09:01:49 +0300

    upd dashboard etc

---
+++ sneaker_store/main/dashboard.py
@@ -0,0 +1,38 @@
+from admin_tools.dashboard import modules, Dashboard
+from .models import ShoeBrand, CategoryModel, ShoeModel
+from django.urls import reverse
+
+
+class CustomTreeModule(modules.DashboardModule):
+    title = 'My Tree Module'
+    template = '../templates/dashboard.html'
+
+    def init_with_context(self, context):
+        brands = ShoeBrand.objects.all()
+        categories = CategoryModel.objects.all()
+        shoes = ShoeModel.objects.all()
+        self.children = [
+            {
+                'brand': brand,
+                'brand_url': reverse('admin:main_shoebrand_change', args=[brand.pk]),
+                'categories': [
+                    {
+                        'category': category,
+                        'category_url': reverse('admin:main_categorymodel_change', args=[category.pk]),
+                        'shoes': [
+                            {
+                                'shoe': shoe,
+                                'shoe_url': reverse('admin:main_shoemodel_change', args=[shoe.pk])
+                            } for shoe in shoes if shoe.parent == category
+                        ]}
+                    for category in categories if category.parent == brand
+                ]
+            } for brand in brands
+        ]
+
+
+class CustomDashboard(Dashboard):
+    def init_with_context(self, context):
+        self.children.append(CustomTreeModule(title='My Tree'))
+
+
