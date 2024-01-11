e5c7bbfe47f7d07dca968401227eefa31c9f83dc
Ruslan <68710700+chebotop@users.noreply.github.com>
2024-01-11 16:56:12 +0300

    merging

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
 
+
 def run_server():
-    subprocess.run('cd sneaker_store && venv\\Scripts\\activate && python manage.py runserver', shell=True)
+    subprocess.run('venv\\Scripts\\activate && cd sneaker_store && python manage.py runserver', shell=True)
+
 
 if __name__ == "__main__":
     run_server()
