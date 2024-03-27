1e28b96001c831547f8ac930bcc7f700240d5a0c
chebotop <chebotop@gmail.com>
2024-03-27 09:45:58 +0300

    preloader added to home page

--- sneaker_store/sneaker_store/settings.py
+++ sneaker_store/sneaker_store/settings.py
@@ -160,7 +160,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# CORS
 CORS_ORIGIN_ALLOW_ALL = True
 # +++++++++++++++++++++++++++
 # +++++++++++++++++++++++++++
